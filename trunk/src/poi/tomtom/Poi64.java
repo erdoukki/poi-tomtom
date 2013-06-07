eaf7011cc2d2229f955a6f515f03c7d9f700f8cb
oritomov <oritomov@8f4352da-a150-5287-13e1-325f6e906070>
2013-06-07 21:16:49 +0000

    fix rec 0x64

--- trunk/src/poi/tomtom/Poi64.java
+++ trunk/src/poi/tomtom/Poi64.java
@@ -1,10 +1,21 @@
 package poi.tomtom;
 
 /**
- * 1 byte      	T:    	type (always 100)
- * 4 bytes     	L:    	length of this record in bytes (including the T and L fields)
- * P bytes     	Name: 	zero-terminated ASCII string specifying the name of the POI type
- * L-P-5 bytes 	Icon: 	zero-terminated ASCII string specifying the name of the .BMP file
+<h3>Record 0x64.</h3>
+This record is a {@link PoiRecord POI Record}.
+<br/>
+The format is the following :
+<p/>
+<table border="1">
+  <tr><th width=110>Bytes</th><th>Description</th></tr>
+  <tr><td>1 byte </td><td>T:  type (always 100)</td></tr>
+  <tr><td>4 bytes</td><td>L:  length of this record in bytes</td></tr>
+  <tr><td>4 bytes</td><td>U1: ???</td></tr>
+  <tr><td>4 bytes</td><td>V:  version</td></tr>
+  <tr><td>3 bytes</td><td>U2: ???</td></tr>
+  <tr><td>1 byte </td><td>C:  check sum</td></tr>
+  <tr><td>4 bytes</td><td>U3: ???</td></tr>
+</table>
  * 
  * @author <a href="mailto:oritomov@yahoo.com">orlin tomov</a>
  */
@@ -12,9 +23,16 @@ public class Poi64 extends PoiCommon implements Poi {
 
 	public static final byte POI64 = 100;
 
-	private int length;
-	private String name;
-	private String icon;
+	public static final int UNKNOWN1 = 4;
+	public static final int UNKNOWN2 = 3;
+	public static final int UNKNOWN3 = 4;
+
+	private int length = 21;
+	private byte[] unknown1 = new byte[UNKNOWN1];
+	private int version;
+	private byte[] unknown2 = new byte[UNKNOWN2];
+	private byte check;
+	private byte[] unknown3 = new byte[UNKNOWN3];
 
 	public Poi64(int type, PoiContainer parent) {
 		super(type, parent);
@@ -25,21 +43,76 @@ public class Poi64 extends PoiCommon implements Poi {
 	public void setLength(int length) {
 		this.length = length;
 	}
-	public String getName() {
-		return name;
+
+	public byte[] getUnknown1() {
+		return unknown1;
 	}
-	public void setName(String name) {
-		this.name = name;
+
+	public void setUnknown1(byte[] unknown1) {
+		this.unknown1 = unknown1;
 	}
-	public String getIcon() {
-		return icon;
+
+	public int getVersion() {
+		return version;
 	}
-	public void setIcon(String icon) {
-		this.icon = icon;
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public byte[] getUnknown2() {
+		return unknown2;
+	}
+
+	public void setUnknown2(byte[] unknown2) {
+		this.unknown2 = unknown2;
+	}
+
+	public int getCheck() {
+		return check;
+	}
+
+	public void setCheck(byte check) {
+		this.check = check;
+	}
+
+	public byte[] getUnknown3() {
+		return unknown3;
+	}
+
+	public void setUnknown3(byte[] unknown3) {
+		this.unknown3 = unknown3;
+	}
+
+	public void doCheck() {
+		int check = getType();
+		// System.out.println(Integer.toHexString(check));
+		for (int i = 0; i < 4; i++) {
+			check += ((size() >> (8 * i)) & 0xff);
+			// System.out.println(Integer.toHexString((size() >> (8*i)) & 0xff));
+			// System.out.println(Integer.toHexString(check));
+		}
+		for (int i = 0; i < UNKNOWN1; i++) {
+			check += (unknown1[i] & 0xff);
+			// System.out.println(Integer.toHexString((unknown1 >> (8*i)) & 0xff));
+			// System.out.println(Integer.toHexString(check));
+		}
+		for (int i = 0; i < 4; i++) {
+			check += ((version >> (8 * i)) & 0xff);
+			// System.out.println(Integer.toHexString((version >> (8*i)) & 0xff));
+			// System.out.println(Integer.toHexString(check));
+		}
+		for (int i = 0; i < UNKNOWN2; i++) {
+			check += (unknown2[i] & 0xff);
+			// System.out.println(Integer.toHexString((unknown2 >> (8*i)) & 0xff));
+			// System.out.println(Integer.toHexString(check));
+		}
+		this.check = (byte) (check & 0xff);
+		// System.out.println(Integer.toHexString(this.check));
 	}
 
 	@Override
 	public String toString() {
-		return "Poi64 [L:" + length + " N:" + name + " I:" + icon + "]";
+		return "Poi64 [L:" + length + " u1:" + hex(unknown1) + " ver:0x" + Integer.toHexString(version) + " u2:" + hex(unknown2) + " chc:0x" + Integer.toHexString(check & 0xff) + " u3:" + hex(unknown3) + "]";
 	}
 }
