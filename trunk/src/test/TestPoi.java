bec6edbebfbe6715e5cf1f065862bb5d1cc6b891
oritomov <oritomov@8f4352da-a150-5287-13e1-325f6e906070>
2013-06-09 15:50:58 +0000

    tests

--- trunk/src/test/TestPoi.java
+++ trunk/src/test/TestPoi.java
@@ -1,28 +1,41 @@
 package test;
 
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
 
 import org.junit.Test;
 
 import poi.tomtom.Poi;
-import poi.tomtom.Poi01;
 import poi.tomtom.PoiInputStream;
 
 public class TestPoi {
 
-	@Test
-	public void test() {
+	@Test 
+	public void testRead01() {
+		byte[] buff = new byte[] {0x01, 0x7F, (byte) 0x8D, 0x00, 0x00, 0x38, (byte) 0xC8, 0x42, 0x00, 0x33, 0x1A, 0x40, 0x00, 0x37, 0x25, 0x28, 0x00};
+		ByteArrayInputStream bais = new ByteArrayInputStream(buff);
+		PoiInputStream is = new PoiInputStream(bais);
+		Poi poi;
 		try {
-			String path = System.getProperty("user.dir");
-			PoiInputStream is = new PoiInputStream(new FileInputStream(path + "/LPG_Greece.ov2"));
-			Poi parent = null;
 			while (is.available() > 0) {
-				Poi poi = is.readPoi();
-				if (poi instanceof Poi01) 
-					parent  = poi;
+				poi = is.readPoi();
 				System.out.println(poi);
 			}
-			System.out.println(parent); //24491
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test 
+	public void testRead02() {
+		byte[] buff = new byte[] {0x02, 0x15, 0x00, 0x00, 0x00, 0x33, 0x1A, 0x40, 0x00, 0x37, 0x25, 0x28, 0x00, 0x73, 0x74, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x00};
+		ByteArrayInputStream bais = new ByteArrayInputStream(buff);
+		PoiInputStream is = new PoiInputStream(bais);
+		Poi poi;
+		try {
+			while (is.available() > 0) {
+				poi = is.readPoi();
+				System.out.println(poi);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
